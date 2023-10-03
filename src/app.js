79c64cf24d8cb09b933afbdcac790ee4f774f080
Vince489 <vincentcornelius489@gmail.com>
2023-10-02 22:55:35 -0400

    app use

--- src/app.js
+++ src/app.js
@@ -20,23 +20,23 @@ app.use(cookieParser());
 app.use("/api/v1", routes);
 
 // Middleware to verify JWT token
-// app.use((req, res, next) => {
-//   // Get the token from the request headers (assuming it's in the 'Authorization' header)
-//   const token = req.headers.authorization || '';
+app.use((req, res, next) => {
+  // Get the token from the request headers (assuming it's in the 'Authorization' header)
+  const token = req.headers.authorization || '';
 
-//   try {
-//     // Verify and decode the token
-//     const decoded = jwt.verify(token, 'your-secret-key'); // Use your actual secret key
+  try {
+    // Verify and decode the token
+    const decoded = jwt.verify(token, 'your-secret-key'); // Use your actual secret key
 
-//     // Attach the decoded user information to the request object for further use
-//     req.user = decoded;
+    // Attach the decoded user information to the request object for further use
+    req.user = decoded;
 
-//     // Continue processing the request
-//     next();
-//   } catch (error) {
-//     // Handle token verification errors (e.g., expired or invalid token)
-//     res.status(401).json({ message: 'Unauthorized' });
-//   }
-// });
+    // Continue processing the request
+    next();
+  } catch (error) {
+    // Handle token verification errors (e.g., expired or invalid token)
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+});
 
 module.exports = app;
